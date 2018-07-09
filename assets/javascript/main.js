90c0c1b4f465baaf0446014d169f84a2c15e3fd0
Guillermo Barila <gui365@yahoo.com.ar>
2018-07-09 09:44:31 -0400

    Defined color palette, populated all sections but Skills

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,7 @@
 // ANIMATIONS
 
 // Smooth scroll animation
-$("#options a").on('click', function(event) {
+$("a").on('click', function(event) {
   if (this.hash !== "") {
     event.preventDefault();
     var hash = this.hash;
@@ -14,8 +14,16 @@ $("#options a").on('click', function(event) {
   }
 });
 
+// Hovering over form button
 $("button").hover(function(){
   $(this).animate({ "border-radius": "20px", "box-shadow": "2px 3px 3px black" }, "fast")
 }, function(){
   $(this).animate({ "border-radius": "0" })
+})
+
+
+$(".portfolio-project").hover(function(){
+  $(this).find(".portfolio-project-links").css( {display: "block"} )
+}, function(){
+  $(this).find(".portfolio-project-links").css( {display: "none"} )
 })
